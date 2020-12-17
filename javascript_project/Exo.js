d8265ee1ceefd631ff54912ce599987f1a451c81
erikshea <71901477+erikshea@users.noreply.github.com>
2020-12-17 16:24:20 +0100

    modif script+ajout html

--- javascript_project/Exo.js
+++ javascript_project/Exo.js
@@ -1,25 +1,19 @@
-
+age();
+1565
 //Modification test2
 function age() {
     var age = prompt("Quel est ton âge?");
-
+    6
     if (age >= 6 && age <= 7) {
         alert("Tu fais partie des poussins")
-    }
-    else if (age == 8 && age <= 9) {
+    } else if (age <= 9) {
         alert("Tu fais partie des Pupille ")
-    }
-    else if (age == 10 && age <= 11) {
+    } else if (age <= 11) {
         alert("Tu fais partie des Minimes")
-    }
-
-    else if (age <= 12 && age <= 17) {
+    } else if (age <= 17) {
         alert("Tu fais partie des cadets")
-    }
-
-    else if (age <= 12 && age <= 17) {
+    } else if (age <= 17) {
         alert("Tu fais partie des cadets")
-    } else (alert("Je ne comprends pas, dis moi quel est ton age?"))
-
+    } else(alert("Je ne comprends pas, dis moi quel est ton age?"))
 
 }
