c60f95093c3185fb1005e527c9fc2174468d8a76
erikshea <71901477+erikshea@users.noreply.github.com>
2020-12-17 17:19:31 +0100

    Modifs exo.js

--- javascript_project/Exo.js
+++ javascript_project/Exo.js
@@ -3,19 +3,21 @@ age();
 //Modification test2 master
 function age() {
     var age = prompt("Quel est ton âge?");
+    let message = "Tu fais partie des ";
     if(age<6){
-        alert("pas de catégorie")
-    }else
-    if (age <= 7) {
-        alert("Tu fais partie des poussins")
+        message = "pas de catégorie";
+    }else if (age <= 7) {
+        message += "poussins";
     } else if (age <= 9) {
-        alert("Tu fais partie des Pupille ")
+        message += "Pupille";
     } else if (age <= 11) {
-        alert("Tu fais partie des Minimes")
+        message += "Minimes";
     } else if (age <= 17) {
-        alert("Tu fais partie des cadets")
+        message += "cadets";
     } else if (age <= 17) {
-        alert("Tu fais partie des cadets")
-    } else(alert("Je ne comprends pas, dis moi quel est ton age?"))
-
+        message = "Tu fais partie des cadets";
+    } else{
+        message = "Je ne comprends pas, dis moi quel est ton age?";
+    }
+    alert(message);
 }
